d55bafb16b8ec522e898a135029ce8934eb7c6e8
c-aksgar <akshi.garg@assaabloy.com>
2019-04-16 21:41:27 +0530

    navigation tab

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+  navLinks: any[];
+  activeLinkIndex = -1;
 
-  constructor() { }
+  constructor() {
+    this.navLinks = [
+      { label: 'Onboarding Form', path: '/onboarding', index: 0 },
+      { label: 'List Students', path: '/student', index: 1 }]
+  }
 
   ngOnInit() {
+  
   }
 
 }

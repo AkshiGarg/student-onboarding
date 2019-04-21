5fe94da9d85a81d83d3806265a598461a329dcb1
c-aksgar <akshi.garg@assaabloy.com>
2019-04-21 19:50:11 +0530

    removing lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './shared/pages/page-not-found/page-not-found.component';
 import { LoginComponent } from './login/login.component';
+import { ListStudentComponent } from './student/list-student.component';
+import { OnboardStudentComponent } from './student/onboard-student.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'student', loadChildren: './student/student.module#StudentModule'},
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'student', component: OnboardStudentComponent },
+  { path: 'student/list', component: ListStudentComponent },
+  // { path: 'student', loadChildren: './student/student.module#StudentModule'},
+  { path: '', redirectTo: '/student', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatButtonModule, MatCardModule, MatCheckboxModule, MatDatepickerModule, MatDialogModule, MatGridListModule, MatIconModule, MatInputModule, MatNativeDateModule, MatOptionModule, MatRadioModule, MatSelectModule, MatSnackBarModule, MatTabsModule, MatToolbarModule } from '@angular/material';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { AppRoutingModule } from './app-routing.module';
-import { LoginModule } from './login/login.module';
-import { SharedModule } from './shared/shared.module';
-
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { InMemoryDataService } from './service/data/data.service';
 import { ConfirmationDialogComponent } from './shared/confirmation-dialog/confirmation-dialog.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { MenuComponent } from './shared/menu/menu.component';
 import { PageNotFoundComponent } from './shared/pages/page-not-found/page-not-found.component';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from './service/data/data.service';
+import { OnboardStudentComponent } from './student/onboard-student.component';
+import { ListStudentComponent } from './student/list-student.component';
+import { StudentDetailComponent } from './student/view-student.component';
+
+
+
 
 @NgModule({
   declarations: [
@@ -22,17 +27,36 @@ import { InMemoryDataService } from './service/data/data.service';
     HeaderComponent,
     PageNotFoundComponent,
     MenuComponent,
-    ConfirmationDialogComponent
+    ConfirmationDialogComponent,
+    LoginComponent,
+    OnboardStudentComponent,
+    ListStudentComponent,
+    StudentDetailComponent
   ],
   imports: [
     CommonModule,
     BrowserModule,
-    LoginModule,
-    SharedModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    HttpClientInMemoryWebApiModule.forFeature(InMemoryDataService)
+    HttpClientInMemoryWebApiModule.forFeature(InMemoryDataService),
+    ReactiveFormsModule,
+    FormsModule,
+    MatRadioModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSnackBarModule,
+    MatCheckboxModule,
+    MatGridListModule,
+    MatDialogModule,
+    MatCardModule,
+    MatTabsModule,
+    MatIconModule
 
   ],
   entryComponents: [
